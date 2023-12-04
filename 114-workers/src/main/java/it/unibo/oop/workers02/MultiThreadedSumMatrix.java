a9696508f67d84ea23395d8655e4b517eda8cd15
Martin Tomassi <martin.tomassi@studio.unibo.it>
2023-12-04 21:15:09 +0100

    Exercise Number 114 Completed

---
+++ 114-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -0,0 +1,69 @@
+package it.unibo.oop.workers02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiThreadedSumMatrix implements SumMatrix{
+    private final int nthread;
+
+    /**
+     * 
+     * @param nthread
+     *            no. of thread performing the sum.
+     */
+    public MultiThreadedSumMatrix(final int nthread) {
+        this.nthread = nthread;
+    }
+
+    private static class Worker extends Thread {
+        private final double[][] matrix;
+        private final int startpos;
+        private final int nelem;
+        private long res;
+
+        public Worker(final double[][] matrix , final int startpos, final int nelem) {
+            super();
+            this.matrix = matrix;
+            this.startpos = startpos;
+            this.nelem = nelem;
+        }
+
+        @Override
+        @SuppressWarnings("PMD.SystemPrintln")
+        public void run() {
+            System.out.println("Working from position " + startpos + " to position " + (startpos + nelem - 1));
+            for (int i = startpos ; i<=(startpos + nelem - 1); i++) {
+                for (int j = 0 ; j<=(this.matrix.length - 1); j++) {
+                    this.res += this.matrix[i][j];
+                }
+            }
+        }
+
+        public long getResult() {
+            return this.res;
+        }
+
+    }
+
+    @Override
+    public double sum(double[][] matrix) {
+        final int size = matrix.length % nthread + matrix.length / nthread;
+        final List<Worker> workers = new ArrayList<>(nthread);
+        for (int start = 0; start < matrix.length; start += size) {
+            workers.add(new Worker(matrix, start, size));
+        }
+        for (final Worker w: workers) {
+            w.start();
+        }
+        long sum = 0;
+        for (final Worker w: workers) {
+            try {
+                w.join();
+                sum += w.getResult();
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+        return sum;
+    }
+}
